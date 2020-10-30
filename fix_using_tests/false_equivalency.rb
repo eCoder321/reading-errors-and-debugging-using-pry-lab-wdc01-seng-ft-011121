d04d02070a67b702d04707e3d499c4d210b6c474
Fem Adisa <femiwonderful@gmail.com>
2020-10-30 03:08:06 +0000

    Done.

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,7 +1,8 @@
 # don't forget to add: require 'pry'
-
+#require 'pry'
 def get_user_input
   gets.chomp
+#  binding.pry
 end
 
 def prompt_user
@@ -11,10 +12,12 @@ def prompt_user
 end
 
 def selection(num)
-  if num = 1
-     "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
-     "HAM HAM HAM IN MY TUMMY"
+  if num == 1
+#    binding.pry
+      "YUM YUM MUNCH MUNCH MUNCH"
+  elsif num == 2
+#  binding.pry
+      "HAM HAM HAM IN MY TUMMY"
   end
 end
 

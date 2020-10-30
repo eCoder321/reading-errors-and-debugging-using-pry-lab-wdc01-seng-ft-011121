cd094d5c86886616a0cab157490e91679a27b140
Fem Adisa <femiwonderful@gmail.com>
2020-10-30 03:05:55 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,16 @@
 # don't forget to add: require 'pry'
+#require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+#    binding.pry
+    s_list = ""
+    10.times do
+      s_list += "s"
+    end
+    s_list + string
   else
+#    binding.pry
     string
   end
 end
